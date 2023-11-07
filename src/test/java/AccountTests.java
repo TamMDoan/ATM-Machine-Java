a57db9a42dcf354d4d55aa8fe970fda0cb26626b
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-07 09:44:45 -0500

    wrote tests up until menu options, figure out how to test those

--- src/test/java/AccountTests.java
+++ src/test/java/AccountTests.java
@@ -61,5 +61,96 @@ public class AccountTests {
 
     }
 
+    @Test
+    public void testCalcCheckingWithdraw(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double withdraw = 536.29;
+        double expected = checkingBalance - withdraw;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        double actual = account.calcCheckingWithdraw(withdraw);
+
+        assertEquals(expected, actual, 2);
+    }
+
+    @Test
+    public void testCalcSavingWithdraw(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double withdraw = 536.29;
+        double expected = savingBalance - withdraw;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        double actual = account.calcSavingWithdraw(withdraw);
+
+        assertEquals(expected, actual, 2);
+    }
+
+    @Test
+    public void testCalcCheckingDeposit(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double deposit = 536.29;
+        double expected = checkingBalance + deposit;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        double actual = account.calcCheckingDeposit(deposit);
+
+        assertEquals(expected, actual, 2);
+    }
+
+    @Test
+    public void testCalcSavingDeposit(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double deposit = 536.29;
+        double expected = savingBalance + deposit;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        double actual = account.calcSavingDeposit(deposit);
+
+        assertEquals(expected, actual, 2);
+    }
+
+    @Test
+    public void testCalcCheckTransfer(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double transfer = 536.29;
+        double expected = savingBalance + transfer;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        account.calcCheckTransfer(transfer);
+
+        assertEquals(expected, account.getSavingBalance(), 2);
+        assertEquals(checkingBalance - transfer, account.getCheckingBalance(), 2);
+    }
+
+    @Test
+    public void testCalcSavingTransfer(){
+        int customerNumber = 123;
+        int pinNumber = 1111;
+        double checkingBalance = 100000.00;
+        double savingBalance = 12345.67;
+        double transfer = 536.29;
+        double expected = checkingBalance + transfer;
+        Account account = new Account(customerNumber, pinNumber, checkingBalance, savingBalance);
+
+        account.calcSavingTransfer(transfer);
+
+        assertEquals(expected, account.getCheckingBalance(), 2);
+        assertEquals(savingBalance - transfer, account.getSavingBalance(), 2);
+    }
 
 }
